c5ef8daea820c708ff6bec340fa9c58fff68510f
Timothy Raymond <xtjraymondx@gmail.com>
2019-01-28 12:58:05 -0500

    Add experiments with module privacy
    I don't think I fully understand it yet, but it was fun to play around
    with. I'm sure this is something the compiler will bite me with later :)

---
+++ ch7/modprivacy/target/debug/deps/modprivacy-78b9cd9dce7a565e.d
@@ -0,0 +1,5 @@
+/Users/timothyraymond/projects/rust/nostarch-rust/ch7/modprivacy/target/debug/deps/libmodprivacy-78b9cd9dce7a565e.rlib: src/lib.rs
+
+/Users/timothyraymond/projects/rust/nostarch-rust/ch7/modprivacy/target/debug/deps/modprivacy-78b9cd9dce7a565e.d: src/lib.rs
+
+src/lib.rs:
